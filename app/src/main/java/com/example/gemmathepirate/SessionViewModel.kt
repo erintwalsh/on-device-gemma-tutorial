b3cdd6a650c20a67176d11616033d3d80ebb08da
Erin Walsh <erintwalsh@google.com>
2025-10-03 15:55:51 -0400

    Adds session management functions to the view model

--- app/src/main/java/com/example/gemmathepirate/SessionViewModel.kt
+++ app/src/main/java/com/example/gemmathepirate/SessionViewModel.kt
@@ -1,11 +1,58 @@
 package com.example.gemmathepirate
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import com.google.ai.edge.litertlm.Engine
+import com.google.ai.edge.litertlm.EngineConfig
+import com.google.ai.edge.litertlm.InputData
+import com.google.ai.edge.litertlm.Session
 
 private const val MODEL_PATH = "/data/local/tmp/llm/gemma_q8_ekv4096.litertlm"
 private const val TAG = "SessionViewModel"
 
+data class LlmModelInstance(val engine: Engine, var session: Session)
+
 class SessionViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var instance: LlmModelInstance
+
+    init {
+        val engineConfig = EngineConfig(
+            modelPath = MODEL_PATH,
+            cacheDir = application.getExternalFilesDir(null)!!.path
+        )
+
+        try {
+            val engine = Engine(engineConfig)
+            engine.initialize()
+            val session = engine.createSession()
+            Log.d(TAG, "Session created")
+
+            instance = LlmModelInstance(engine, session)
+        } catch (e: Exception) {
+            Log.d(TAG, "Exception thrown during session creation: ${e.message}")
+            throw Exception("Unable to initialize SessionViewModel")
+        }
+    }
+
+    suspend fun startInference(inputText: String): String {
+        val inputData = mutableListOf<InputData>()
+        inputData.add(InputData.Text(inputText))
+        return instance.session.generateContent(inputData)
+    }
+
+    fun cleanUp() {
+        try {
+            instance.session.close()
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to close the session: ${e.message}")
+        }
+
+        try {
+            instance.engine.close()
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to close the engine: ${e.message}")
+        }
+    }
 }
